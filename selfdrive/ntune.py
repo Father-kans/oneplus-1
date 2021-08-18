c4826de4b9d833a7bc35990be085932ccee747cd
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-08-18 11:30:03 +0900

    Revert "Update ntune.py"
    This reverts commit f0c730df1895783c1b7adc8a5d6bc4ed68304d2f.

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -3,25 +3,23 @@ import fcntl
 import signal
 import json
 import numpy as np
+from common.realtime import DT_CTRL
 
 CONF_PATH = '/data/ntune/'
+CONF_COMMON_FILE = '/data/ntune/common.json'
 CONF_LQR_FILE = '/data/ntune/lat_lqr.json'
+CONF_INDI_FILE = '/data/ntune/lat_indi.json'
 
-ntunes = {}
-
-def file_watch_handler(signum, frame):
-  global ntunes
-  for ntune in ntunes.values():
-    ntune.handle()
 
 class nTune():
-  def __init__(self, CP=None, controller=None, group=None):
+  def __init__(self, CP=None, controller=None):
 
     self.invalidated = False
+
     self.CP = CP
+
     self.lqr = None
-    self.group = group
-    self.config = {}
+    self.indi = None
 
     if "LatControlLQR" in str(type(controller)):
       self.lqr = controller
@@ -31,8 +29,11 @@ class nTune():
       self.lqr.C = np.array([1., 0.]).reshape((1, 2))
       self.lqr.K = np.array([-110., 451.]).reshape((1, 2))
       self.lqr.L = np.array([0.33, 0.318]).reshape((2, 1))
+    elif "LatControlINDI" in str(type(controller)):
+      self.indi = controller
+      self.file = CONF_INDI_FILE
     else:
-      self.file = CONF_PATH + group + ".json"
+      self.file = CONF_COMMON_FILE
 
     if not os.path.exists(CONF_PATH):
       os.makedirs(CONF_PATH)
@@ -40,7 +41,7 @@ class nTune():
     self.read()
 
     try:
-      signal.signal(signal.SIGIO, file_watch_handler)
+      signal.signal(signal.SIGIO, self.handler)
       fd = os.open(CONF_PATH, os.O_RDONLY)
       fcntl.fcntl(fd, fcntl.F_SETSIG, 0)
       fcntl.fcntl(fd, fcntl.F_NOTIFY, fcntl.DN_MODIFY | fcntl.DN_CREATE | fcntl.DN_MULTISHOT)
@@ -48,19 +49,19 @@ class nTune():
       print("exception", ex)
       pass
 
-  def handle(self):
+  def handler(self, signum, frame):
     try:
-      if os.path.getsize(self.file) > 0:
+      if os.path.isfile(self.file):
         with open(self.file, 'r') as f:
           self.config = json.load(f)
+          if self.checkValid():
+            self.write_config(self.config)
 
-        if self.checkValid():
-          self.write_config(self.config)
-
-        self.invalidated = True
-
-    except:
+    except Exception as ex:
+      print("exception", ex)
       pass
+
+    self.invalidated = True
 
   def check(self):  # called by LatControlLQR.update
     if self.invalidated:
@@ -68,29 +69,28 @@ class nTune():
       self.update()
 
   def read(self):
-    success = False
+
     try:
-      if os.path.getsize(self.file) > 0:
+      if os.path.isfile(self.file):
         with open(self.file, 'r') as f:
           self.config = json.load(f)
+          if self.checkValid():
+            self.write_config(self.config)
 
-        if self.checkValid():
-          self.write_config(self.config)
           self.update()
-        success = True
-    except:
-      pass
-
-    if not success:
-      try:
+      else:
         self.write_default()
+
         with open(self.file, 'r') as f:
           self.config = json.load(f)
           if self.checkValid():
             self.write_config(self.config)
           self.update()
-      except:
-        pass
+
+    except:
+      return False
+
+    return True
 
   def checkValue(self, key, min_, max_, default_):
     updated = False
@@ -111,15 +111,17 @@ class nTune():
 
     if self.lqr is not None:
       return self.checkValidLQR()
-    elif self.group == "common":
-      return self.checkValidCommon()
+    elif self.indi is not None:
+      return self.checkValidINDI()
     else:
-      return self.checkValidISCC()
+      return self.checkValidCommon()
 
   def update(self):
 
     if self.lqr is not None:
       self.updateLQR()
+    elif self.indi is not None:
+      self.updateINDI()
 
   def checkValidCommon(self):
     updated = False
@@ -158,16 +160,22 @@ class nTune():
 
     return updated
 
-  def checkValidISCC(self):
+  def checkValidINDI(self):
     updated = False
 
-    if self.checkValue("sccGasFactor", 0.5, 1.5, 1.0):
+    if self.checkValue("innerLoopGain", 0.5, 10.0, 3.3):
       updated = True
 
-    if self.checkValue("sccBrakeFactor", 0.5, 1.5, 1.0):
+    if self.checkValue("outerLoopGain", 0.5, 10.0, 2.7):
       updated = True
 
-    if self.checkValue("sccCurvatureFactor", 0.5, 1.5, 1.0):
+    if self.checkValue("timeConstant", 0.1, 5.0, 2.0):
+      updated = True
+
+    if self.checkValue("actuatorEffectiveness", 0.1, 5.0, 1.7):
+      updated = True
+
+    if self.checkValue("steerLimitTimer", 0.5, 3.0, 0.8):
       updated = True
 
     return updated
@@ -184,7 +192,21 @@ class nTune():
     self.lqr.x_hat = np.array([[0], [0]])
     self.lqr.reset()
 
+  def updateINDI(self):
+
+    self.indi.RC = float(self.config["timeConstant"])
+    self.indi.G = float(self.config["actuatorEffectiveness"])
+    self.indi.outer_loop_gain = float(self.config["outerLoopGain"])
+    self.indi.inner_loop_gain = float(self.config["innerLoopGain"])
+    self.indi.alpha = 1. - DT_CTRL / (self.indi.RC + DT_CTRL)
+
+    self.indi.sat_limit = float(self.config["steerLimitTimer"])
+
+    self.indi.reset()
+
   def read_cp(self):
+
+    self.config = {}
 
     try:
       if self.CP is not None:
@@ -195,6 +217,15 @@ class nTune():
           self.config["dcGain"] = round(self.CP.lateralTuning.lqr.dcGain, 6)
           self.config["steerLimitTimer"] = round(self.CP.steerLimitTimer, 2)
           self.config["steerMax"] = round(self.CP.steerMaxV[0], 2)
+
+        elif self.CP.lateralTuning.which() == 'indi' and self.indi is not None:
+          self.config["innerLoopGain"] = round(self.CP.lateralTuning.indi.innerLoopGain, 2)
+          self.config["outerLoopGain"] = round(self.CP.lateralTuning.indi.outerLoopGain, 2)
+          self.config["timeConstant"] = round(self.CP.lateralTuning.indi.timeConstant, 2)
+          self.config["actuatorEffectiveness"] = round(self.CP.lateralTuning.indi.actuatorEffectiveness, 2)
+          self.config["steerLimitTimer"] = round(self.CP.steerLimitTimer, 2)
+          self.config["steerMax"] = round(self.CP.steerMaxV[0], 2)
+
         else:
           self.config["useLiveSteerRatio"] = 1.
           self.config["steerRatio"] = round(self.CP.steerRatio, 2)
@@ -217,7 +248,7 @@ class nTune():
     try:
       with open(self.file, 'w') as f:
         json.dump(conf, f, indent=2, sort_keys=False)
-        os.chmod(self.file, 0o666)
+        os.chmod(self.file, 0o764)
     except IOError:
 
       try:
@@ -226,16 +257,15 @@ class nTune():
 
         with open(self.file, 'w') as f:
           json.dump(conf, f, indent=2, sort_keys=False)
-          os.chmod(self.file, 0o666)
+          os.chmod(self.file, 0o764)
       except:
         pass
 
-def ntune_get(group, key):
-  global ntunes
-  if group not in ntunes:
-    ntunes[group] = nTune(group=group)
-
-  ntune = ntunes[group]
+ntune = None
+def ntune_get(key):
+  global ntune
+  if ntune == None:
+    ntune = nTune()
 
   if ntune.config == None or key not in ntune.config:
     ntune.read()
@@ -248,11 +278,5 @@ def ntune_get(group, key):
 
   return v
 
-def ntune_common_get(key):
-  return ntune_get("common", key)
-
-def ntune_common_enabled(key):
-  return ntune_common_get(key) > 0.5
-
-def ntune_scc_get(key):
-  return ntune_get("scc", key)
+def ntune_isEnabled(key):
+  return ntune_get(key) > 0.5
